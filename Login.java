c3da00e5c80e380f6ad0822147b8f16e2d34be1c
shankar wangwad <shankarwangwad2018@gmail.com>
2021-10-25 22:58:12 +0530

    basic setup

---
+++ Login.java
@@ -0,0 +1,86 @@
+package Credentials;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import Requirements.Account;
+
+public class Login extends Account{
+
+    public Login()
+    {
+        boolean flag1=false;
+        boolean flag2=false;
+
+        System.out.print("\n\t\tEnter your name : ");
+        username=str.nextLine();
+        System.out.print("\n\t\tEnter your Password : ");
+        password=str.nextLine();
+        
+  
+
+
+        String f=username;
+        f=f+".txt";
+       try{
+
+          FileReader fr=new FileReader(f);
+          BufferedReader bfr=new BufferedReader(fr);
+          
+            String name=bfr.readLine();
+            String ps=bfr.readLine();
+
+             bfr.close();
+           
+             name=name.trim();
+          char arr[]=name.toCharArray();
+          char pas[]=ps.toCharArray();
+          for(int i=0;i<username.length();i++)
+          {
+              if(arr[i]!=username.charAt(i))
+              {
+                  flag1=true;break;
+              }
+          }
+          for(int i=0;i<password.length();i++)
+          {
+              if(pas[i]!=password.charAt(i))
+              {
+                  flag2=true;
+                  break;
+              }
+          }
+
+         if(!flag1&&!flag2)
+         {
+             System.out.print("\n\t\tLogged in Succesfully");
+
+
+             // steps after logged in to be performed  ---------------------->
+
+
+
+         }
+        else{
+    
+             System.out.print("\n\t\tInvalid Credentials ..........");
+        }
+
+
+
+
+       }
+       catch(Exception e)
+       {
+           System.out.print("\n\t\tNo account found with your name ."); 
+        
+        }
+        
+
+    
+        
+        
+        
+        
+    }
+}
---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import Requirements.Account;
+import Credentials.Login;
+public class Main{
+
+    public static void main(String[] args)
+    {
+        int res;         
+        String pass;
+        String us;
+       // String name;
+       
+       Scanner sc=new Scanner(System.in);        //for taking integer input
+       Scanner str=new Scanner(System.in);       // for taking String input
+      
+       System.out.println("\n\t\tWELCOME to Student corporate bank");
+       System.out.println("\n\n\t\t Select one option from below");
+       System.out.println("\n\t\t1 : Continue/login as Admin ");
+       System.out.println("\t\t2 : Continue/login as User ");
+       System.out.println("\t\t3 : Create new Account ");
+       System.out.print("\t\tYour response : ");
+      
+       res=sc.nextInt();
+       System.out.println("\n\t\t*****************************************");
+       switch (res) {
+           case 1:
+             
+             /*  System.out.print("\n\n\t\tEnter your username : ");
+               us=str.nextLine();
+               System.out.print("\n\t\tEnter your password(numeric and with less than 3 digits) : ");
+                pass=str.next(); */
+                
+                Login user=new Login();
+               break;
+          
+         case 2:
+               System.out.print("\n\n\t\tEnter your username : ");
+               us=str.nextLine();
+               System.out.print("\t\tEnter your password(numeric and with less than 3 digits) : ");
+               pass=str.next();
+               break;
+
+          case 3:
+            
+             Account newAccount=new Account(1);
+             break;
+
+           default:
+               System.out.println("\n\t\tNot a valid response ");
+               System.out.println("\n\t\tThanks for visiting us");
+               break;
+       } 
+
+    
+
+    }
+}
+
